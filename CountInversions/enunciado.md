9c115f63b462087c20c4db364e838d7107fd1c9e
Julio Dourado <juliodourado419@gmail.com>
2024-12-21 11:18:37 -0300

    doc count inversions

--- CountInversions/enunciado.md
+++ CountInversions/enunciado.md
@@ -1,11 +1,11 @@
-## 3193. Conte o número de inversões
+# 3193. Conte o Número de Inversões
 
-Você recebe um inteiro ne uma matriz 2D requirements, onde representa o índice final e a contagem de inversão de cada requisito.requirements[i] = [endi, cnti]
+Você recebe um inteiro `n` e uma matriz 2D `requirements`, onde `requirements[i] = [endi, cnti]` representa o índice final e a contagem de inversões de cada requisito.
 
-Um par de índices (i, j)de uma matriz de inteiros numsé chamado de inversão se:
+Um par de índices `(i, j)` de uma matriz de inteiros `nums` é chamado de inversão se:
 
-i < jenums[i] > nums[j]
-Retorna o número de
-permutações
- permde [0, 1, 2, ..., n - 1]tal forma que para todo requirements[i] , tem exatamente inversões.perm[0..endi]cnti
- Como a resposta pode ser muito grande, retorne-a módulo .109 + 7
+- `i < j` e `nums[i] > nums[j]`.
+
+Retorne o número de **permutações** `perm` de `[0, 1, 2, ..., n - 1]` tal que, para todo `requirements[i]`, o prefixo `perm[0..endi]` tenha exatamente `cnti` inversões.
+
+Como a resposta pode ser muito grande, retorne-a módulo \(10^9 + 7\).
--- CountInversions/teste1.py
+++ CountInversions/teste1.py
@@ -2,22 +2,32 @@ class Solution(object):
     def numberOfPermutations(self, n, requirements):
         MOD = 10**9 + 7
         
-        # Descobrir o maximo de inversoess necessario
+        req_map = {}
         max_inv = 0
         for end_i, cnt_i in requirements:
+            req_map[end_i+1] = cnt_i
             if cnt_i > max_inv:
                 max_inv = cnt_i
         
-        ## dp[i][c] será a quantidade de permutações de tamanho i com c inversoes
+        # dp[i][c]: numero de permutacoes de tamanho i com c inversoes
         dp = [[0]*(max_inv+1) for _ in range(n+1)]
-        dp[0][0] = 1 
+        dp[0][0] = 1
         
         for i in range(1, n+1):
-            for c in range(max_inv+1):
-                dp[i][c] = dp[i-1][c]
-  
-        print("DP gerada (incompleta):")
-        for i in range(n+1):
-            print("i =", i, ":", dp[i])
+            dp[i][0] = dp[i-1][0]
+            for c in range(1, max_inv+1):
+                val = dp[i][c-1] + dp[i-1][c]
+                if c - i >= 0:
+                    val -= dp[i-1][c - i]
+                dp[i][c] = val % MOD
+            
+            if i in req_map:
+                required = req_map[i]
+                keep = dp[i][required]
+                for c in range(max_inv+1):
+                    dp[i][c] = 0
+                dp[i][required] = keep
+            
+            print("dp após filtrar i =", i, ":", dp[i])
         
-        return dp[n][0] % MOD
+        return dp[n][req_map[n]] % MOD
